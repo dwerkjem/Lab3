7ddc983079a395ab6a48fa0c6b34627574ebe5be
Dwerkjem <derekrneilson@gmail.com>
2026-05-28 21:55:11 -0600

    Added doc string to make_or_select_existing_autocomplete

--- src/lab3/modules/crud.py
+++ src/lab3/modules/crud.py
@@ -2,6 +2,8 @@ import questionary
 import sqlite3
 from collections.abc import Callable
 
+from typing import Any
+
 
 class CRUD:
     def __init__(self, db_path: str):
@@ -10,7 +12,9 @@ class CRUD:
         self.cursor = self.conn.cursor()
 
     def create_room(self, room_name: str, capacity: int, day_rate_cents: int):
-        print(self.db_path)
+        CRUD.make_or_select_existing_autocomplete(
+            self,
+        )
 
     def make_or_select_existing_autocomplete(
         self,
@@ -19,7 +23,19 @@ class CRUD:
         table: str,
         autocomplete_prompt: str,
         validator: Callable,
-    ) -> dict[str, int | bool] | None:
+    ) -> dict[str, str | bool] | None:
+        """Asks for a input and autocompletes based on whether it exists if it dose it selects it and returns a dictionary with its name and whether it existed or not in `"value"` and `"existed"` respectively
+
+        Args:
+            id_col (str): What the id is called in the data base
+            row_col (str): What value are you searching for.
+            table (str): In what table are you searching.
+            # autocomplete_prompt (str): What should the prompt be.
+            validator (Callable): A Validator function that must take 1 positional argument that is to be validated and return a str if invalid and a bool of True if valid.
+
+        Returns:
+            dict[str, Any | bool] | None: A dictionary with `"value"` as the inserted/selected value and a boolean value of whether it existed or not. it can also return `None` if invalid.
+        """
 
         query = f"SELECT {id_col}, {row_col} FROM {table}"
         self.cursor.execute(query)
